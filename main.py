ac1fbbda78798f400e3de9497c6024034fa2ea9f
Charles <creidbrowniii@gmail.com>
2024-06-19 00:14:55 -0500

    gotta work on error handling :P

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, abort
 from dataclasses import dataclass
 
 app = Flask(__name__)
@@ -7,26 +7,66 @@ app = Flask(__name__)
 class Contact:
     name: str
     email: str
+    id: int
 
-def newContact(name, email) -> Contact:
-    return Contact(name, email)
+class Contacts:
+    def __init__(self):
+        self.contacts: list[Contact] = []
+        self.id: int = 0
 
-Contacts: list[Contact] = [newContact("John", "jd@gmail.com"),
-                           newContact("Clara", "cd@gmail.com")]
+    def newContact(self, name, email) -> Contact:
+        self.id += 1
+        new_contact = Contact(name, email, self.id)
+        self.contacts.append(new_contact)
+        return new_contact
+    
+    def removeContact(self, id) -> bool:
+        index = self.indexOf(id)
+        if index != -1:
+            del self.contacts[index]
+            return True
+        else:
+            return False
+
+    def indexOf(self, id) -> int:
+        for i, c in enumerate(self.contacts):
+            if c.id == id:
+                return i
+        return -1
+
+    def get_contacts(self) -> list[Contact]:
+        return self.contacts
+
+contact_list = Contacts()
+contacts = contact_list.get_contacts()
 
 @app.route("/")
 def main():
-    return render_template("index.html", Contacts=Contacts) 
+    return render_template("index.html", contacts=contacts) 
 
 @app.route("/contacts", methods=['POST'])
 def process():
     name = request.form.get("name")
     email = request.form.get("email")
-    new_contact = newContact(name, email)
 
-    Contacts.append(new_contact)
+    if any(contact.email == email for contact in contacts):
+        abort(422, description="Email already exists...")
 
-    return render_template("contact.html", Contacts=Contacts)
+    contact_list.newContact(name, email) 
+
+    return render_template("contacts.html", contacts=contacts)
+
+@app.route("/contacts/<int:id>", methods=['DELETE'])
+def delete_contact(id):
+    if not contact_list.removeContact(id):
+        return "contact not found...", 404
+    contacts = contact_list.get_contacts()
+    return render_template("contacts.html", contacts=contacts)
+
+@app.errorhandler(422)
+def bad_request(error):
+    render_template("contacts.html", contacts=contacts)
+    return render_template("form.html", error=error.description), 422
 
 # run app
 if __name__ == "__main__":
